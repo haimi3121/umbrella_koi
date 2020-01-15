3a7e0b9b8923c3074ce7a302200cbcd43417e186
haimi3121 <haimi3121@gmail.com>
2020-01-15 22:16:15 +0900

    152216

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "tWzjhFGLj4TVwNkzR7vSw4pwN", "hbgltW8wOkdZVsjLbBi00zxvlWVsZ5Cn8IpxKSjAJEICncrs50"
+  provider :twitter, ENV['APP_ID'], ENV['APP_SECRET']
 end
