c2704ef937ca6cfc2f1eba5fb7ea1a29005e4439
haimi3121 <haimi3121@gmail.com>
2020-01-25 21:26:24 +0900

    0125

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -2,7 +2,7 @@ class TimersController < ApplicationController
   def time
   @userdates2  = User.find_by(twitter: session[:twitter])
 session[:userdate2] = (Date.today - Date.parse(@userdates2.updated_at.to_s)).to_i
-if session[:userdate2] < 1
+if session[:userdate2] < -1
 session[:userdate3] = 1
 session[:content2] = @userdates2.username
 session[:content1] = nil
